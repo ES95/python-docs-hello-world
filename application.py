929edae831aef6ca286713fdbeae964b59f1f863
EKLAVYA SHARMA <ekyusharma@gmail.com>
2020-02-03 19:43:20 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,8 +3,11 @@ from flask import request
 import requests,json
 app = Flask(__name__)
 
+app.config["DEBUG"] = True
+
 @app.route("/")
 def hello():
+    return "s"
     text = str(request.args.get('text'))
     main_url = "https://luis-final.cognitiveservices.azure.com/luis/prediction/v3.0/apps/07c0c9aa-7815-4e09-92da-787ab7310d20/slots/staging/predict?subscription-key=3050a2b8146f4ea987465805faa49592&verbose=true&show-all-intents=true&log=true&query="
     url=main_url+text
