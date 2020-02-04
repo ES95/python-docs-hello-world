788e25c82193fa9f4ca8b9b6b87d39870b4d8021
EKLAVYA SHARMA <ekyusharma@gmail.com>
2020-02-04 16:35:23 +0530

    Update application.py

--- application.py
+++ application.py
@@ -7,6 +7,8 @@ app.config["DEBUG"] = True
 @app.route("/")
 def hello():
     import requests,json
+    text = str(request.args.get('text'))
+    debug=False
     resp={}
     main_url = "https://luis-final.cognitiveservices.azure.com/luis/prediction/v3.0/apps/07c0c9aa-7815-4e09-92da-787ab7310d20/slots/staging/predict?subscription-key=3050a2b8146f4ea987465805faa49592&verbose=true&show-all-intents=true&log=true&query="
     trailing_subs=[" go to the"," in the"," of the", " in"," and" ," and then"," on the"," on"," then"," to"," from"," instead"]
